19540c19d3fe28478b56ecd4f25b9a25dfd29747
Zhan BingCong 詹炳聪 <bingco.zn@gmail.com>
2020-10-12 15:00:59 +0800

    试试cdn

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,5 @@
 module.exports = {
   runtimeCompiler: true,
-  publicPath: process.env.NODE_ENV === 'production' ? 'http://cdn.abc.com/' : '',
+  publicPath: process.env.NODE_ENV === 'production' ? 'https://cdn.jsdelivr.net/gh/bingco-zhan/vue-form-dragger@0.0.1/dist/' : '',
   assetsDir: 'static'
 }
