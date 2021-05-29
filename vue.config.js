5b9102c9a29ba1999c0e0268886b72a3cf9cb485
unknown <bingco.zn@gmail.com>
2021-05-29 21:39:06 +0800

    改回github路径

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    // publicPath: process.env.NODE_ENV === 'production' ? '/vue-form-dragger/dist/' : '',
-    publicPath: process.env.NODE_ENV === 'production' ? '/learn-workspace/vue/workspace/coding1/vue-form-dragger/dist/' : '',
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-form-dragger/dist/' : '',
     css: {
         loaderOptions: {
             less: {
