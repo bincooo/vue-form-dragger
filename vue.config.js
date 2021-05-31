18cfc970a5a2826c43de91f905ad9132bcbd68b2
Zhan BingCong 詹炳聪 <bingco.zn@gmail.com>
2021-05-31 15:12:03 +0800

    '组件库打包配置'

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,32 @@
+const path = require("path")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
+let PUBLIC_PATH = (function() {
+  const GITHUB_PAGE_PATH = "/vue-form-dragger/dist/"
+  return process.env.NODE_ENV === "production" ? GITHUB_PAGE_PATH : ""
+})()
+
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/vue-form-dragger/dist/" : "",
-  // css: {
-  //   loaderOptions: {
-  //     less: {
-  //       globalVars: {
-  //         hack: `true; @import '~@/assets/style/var.less';`
-  //       }
-  //     }
-  //   }
-  // },
+  publicPath: PUBLIC_PATH,
   pages: {
     index: {
       entry: "examples/main.ts",
       template: "public/index.html",
       filename: "index.html"
+    }
+  },
+  configureWebpack: (config) => {
+    // 打包组件库环境
+    if (process.env.NODE_ENV === "library") {
+      return {
+        plugins: [
+          new CopyWebpackPlugin([
+            {
+              from: path.resolve(__dirname, "src/style"),
+              to: path.resolve(__dirname, "lib/style")
+            }
+          ])
+        ]
+      }
     }
   }
 }
